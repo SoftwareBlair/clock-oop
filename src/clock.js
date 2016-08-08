cbf298e108ee83788bbfd2955d431e56373400d5
Brandon Blair <brandon.d.blair@gmail.com>
2016-08-07 23:09:26 -0600

    Still need to finish test and some of the functions

--- src/clock.js
+++ src/clock.js
@@ -1,1 +1,69 @@
 // your clock code here
+function Clock(options) {
+  // console.log(options);
+  options = options || {};
+  this.hours = options.hours || 0;
+  this.minutes = options.minutes || 0;
+  this.seconds = options.seconds || 0;
+  this.twentyFour = '24';
+  this.amPm = 'AM';
+}
+
+// Get and Set Hours Methods
+Clock.prototype.getHours = function() {
+  return this.hours;
+}
+
+Clock.prototype.setHours = function(hour) {
+  if (hour >= 12 && hour < 24) {
+    this.hours = checkHour(hour);
+    this.twentyFour = '24';
+    this.amPm = 'PM';
+  } else if (hour >= 0 && hour <= 12) {
+    this.hours = checkHour(hour);
+  }
+}
+
+// Get and Set Minutes Methods
+Clock.prototype.getMinutes = function() {
+  return this.minutes;
+}
+
+Clock.prototype.setMinutes = function(minutes) {
+  this.minutes = checkMinute(minutes);
+}
+
+// Get and Set Seconds Methods
+Clock.prototype.getSeconds = function() {
+  return this.seconds;
+}
+
+Clock.prototype.setSeconds = function(seconds) {
+  this.seconds = checkSeconds(seconds);
+}
+
+Clock.prototype.getTime = function() {
+  if (this.twentyFour === '24') {
+    return this.hour + ':' + this.minutes + ':' + this.seconds;
+  } else {
+    return this.hour + ':' + this.minutes + ':' + this.seconds + ' ' + this.amPm;
+  }
+}
+
+var clock = new Clock()
+
+// Helper functions
+
+function checkHour(hour) {
+  (hour !== undefined) ? ((hour > 00 && hour < 24) ? return hour) : return 00;
+}
+
+function checkMinute(minutes) {
+  (minut !== undefined) ? ((minutes >= 00 && minutes < 60) ? return minutes) : return 00;
+}
+
+function checkSeconds(seconds) {
+  (seconds !== undefined) ? ((seconds >= 00 && seconds < 60) ? return seconds) : return 00;
+}
+
+module.exports = Clock;
--- test/clock.test.js
+++ test/clock.test.js
@@ -1,1 +1,80 @@
-// your test code here
+(function() {
+
+  'use strict';
+
+  // your test code here
+  var Clock = require('../src/clock.js');
+  var chai = require('chai');
+  var expect = chai.expect;
+
+  // Test for Clock initialization
+  describe('new Clock()', function() {
+    it('Should be set to midnight', function() {
+      var clock = new Clock();
+      expect(clock.hours).to.equal(0);
+      expect(clock.minutes).to.equal(0);
+      expect(clock.seconds).to.equal(0);
+    });
+    it('Should take an argument that manually sets the clock', function() {
+      var clock = new Clock({
+        hours: 16,
+        minutes: 30,
+        seconds: 15
+      });
+      expect(clock.hours).to.equal(16);
+      expect(clock.minutes).to.equal(30);
+      expect(clock.seconds).to.equal(15);
+    });
+  });
+
+  // Testing to Get and Set Hours
+  describe('getHours()', function() {
+    xit('Should return the current hours for the clock', function() {
+      var clock = new Clock({
+        hours: 14
+      });
+      expect(clock.getHours()).to.equal(14);
+    });
+  });
+
+  describe('setHours()', function() {
+    xit('Should set the hours', function() {
+      var clock = new Clock();
+      clock.setHours(10);
+      expect(clock.getHours()).to.equal(10);
+    });
+  });
+
+  // Testing to Get and Set Minutes
+  describe('getMinutes()', function() {
+    xit('Should return the current minutes for the clock', function() {
+      var clock = new Clock();
+      expect(clock.getMinutes()).to.equal();
+    });
+  });
+
+  describe('setMinutes()', function() {
+    xit('Should set the minutes', function() {
+      var clock = new Clock();
+      clock.setMinutes();
+      expect(clock.getMinutes()).to.equal();
+    });
+  });
+
+  // Testing to Get and Set Seconds
+  describe('getSeconds()', function() {
+    xit('Should return the current seconds for the clock', function() {
+      var clock = new Clock();
+      expect(clock.getSeconds()).to.equal();
+    });
+  });
+
+  describe('setSeconds()', function() {
+    xit('Should set the seconds', function() {
+      var clock = new Clock();
+      clock.setSeconds();
+      expect(clock.getSeconds()).to.equal();
+    });
+  });
+
+}());
